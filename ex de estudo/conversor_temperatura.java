8aeb97ef41cc04dc45b5551246e4b9c8f2789128
Thiago_Wingerter <thiagowingerter@hotmail.com>
2019-12-23 16:50:30 -0300

    retired not used class.

--- ex de estudo/conversor_temperatura.java
+++ ex de estudo/conversor_temperatura.java
@@ -1,13 +1,10 @@
-public class Solution {
-    public static void main(String[] args) {
-        System.out.println(convertCelsiusToFahrenheit(41));
-    }
+public static void main(String[] args) {
+    System.out.println(convertCelsiusToFahrenheit(41));
+}
 
-    public static double convertCelsiusToFahrenheit(int celsius) {
-        //escreva seu código aqui
-        double tf = 0;
+public static double convertCelsiusToFahrenheit(int celsius) {
+    double tf = 0;
         
-        tf = (9 / 5.0) * celsius + 32;
-        return tf;
-    }
+    tf = (9 / 5.0) * celsius + 32;
+    return tf;
 }
