981562b46f7186e309b287e11b8596ba2508d70a
Thiago_Wingerter <thiagowingerter@hotmail.com>
2019-12-23 17:07:54 -0300

    add Using_objects.java

---
+++ ex de estudo/Using_objects.java
@@ -0,0 +1,20 @@
+public class Using_objects {
+    public static void main(String[] args) {
+        Man man = new Man();
+        Woman woman = new Woman();
+        man.wife = woman;
+        woman.husband = man;
+    }
+
+    public static class Man {
+        public int age;
+        public int height;
+        public Woman wife;
+    }
+
+    public static class Woman {
+        public int age;
+        public int height;
+        public Man husband;
+    }
+}
